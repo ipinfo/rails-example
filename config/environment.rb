b69e76e4968ff9d932f4ddfde70ab099376e2466
troysk <shirsendu.karmakar@gmail.com>
2021-07-27 16:24:58 +0530

    Enable IPinfo gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+require 'ipinfo-rails'
+Rails.application.config.middleware.use(IPinfoMiddleware)
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
